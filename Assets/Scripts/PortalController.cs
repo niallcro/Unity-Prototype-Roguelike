36e96d3fd791f4ffd6a874b0934b889c3556723c
Niall Crowley <niall.crowley@salesforce.com>
2018-07-25 23:17:48 +0100

    Added portals and a second floor

---
+++ Assets/Scripts/PortalController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalController : MonoBehaviour {
+
+  public PortalController connectedPortal;
+
+  void OnTriggerEnter2D(Collider2D other) {
+      other.transform.position = connectedPortal.transform.position + new Vector3(transform.localScale.x, transform.localScale.x);
+  }
+}
