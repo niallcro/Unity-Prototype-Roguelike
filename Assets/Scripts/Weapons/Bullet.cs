49cb52dfa1b889ca8db3e8588a89381ef8c0dcc9
Niall Crowley <niall.crowley@salesforce.com>
2018-07-31 22:37:34 +0100

    Firing bullets towards mouse pointer

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -7,16 +7,18 @@ public class Bullet : MonoBehaviour {
   public float damage;
   public float bulletSpeed;
   Vector3 target;
+  Vector2 direction;
 
 	// Use this for initialization
 	void Start () {
-    Vector3 shootDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-    GetComponent<Rigidbody2D>().velocity = shootDirection * bulletSpeed;
+    Vector3 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+    direction = target - transform.position;
+    direction.Normalize();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-    
+    GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
 	}
 
   private void OnTriggerEnter2D(Collider2D other) {
--- Assets/Scripts/Weapons/Handgun.cs
+++ Assets/Scripts/Weapons/Handgun.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 public class Handgun : Weapon {
 
   public Bullet bullet;
+  Transform muzzle;
+
+	protected override void Start () {
+    base.Start();
+    muzzle = transform.Find("Muzzle");
+  }
 
   public override void UseWeapon() {
     print("Using " + gameObject);
-    Instantiate(bullet, transform.position, Quaternion.identity);
+    Instantiate(bullet, muzzle.position, Quaternion.identity);
   }
 }
